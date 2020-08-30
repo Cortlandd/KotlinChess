9585567bf40d56bcf2e37de5bc9017f50d546735
Cortland Walker <cortlandjwalker@icloud.com>
2020-08-30 12:32:35 -0500

    Draw ChessPieces on to Board

--- Engine/src/main/java/com/cortland/kotlinchess/BoardView.kt
+++ Engine/src/main/java/com/cortland/kotlinchess/BoardView.kt
@@ -1,7 +1,10 @@
 package com.cortland.kotlinchess
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.MotionEvent
@@ -11,10 +14,9 @@ class BoardView(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
 
     var boardViewListener: BoardViewListener? = null
 
-    private val COLS = 8
-    private val ROWS = 8
-
-    private var mBoardLocations: Array<Array<BoardLocation>>
+    var mPieceViews = ArrayList<PieceView>()
+    var selectedIndex: Int? = null
+        set(value) = updatePieceViewSelectedStates()
 
     var x0 = 0
     var y0 = 0
@@ -24,26 +26,56 @@ class BoardView(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
     var flipped = false
 
     init {
-        this.mBoardLocations = Array(COLS, {
-            Array(ROWS, {
-                BoardLocation(0, 0)
-            })
-        })
 
-        setFocusable(true)
-        buildTiles()
+        isFocusable = true
+        buildBoardLocations()
     }
 
-    fun buildTiles() {
-        for (c in 0 until COLS) {
-            for (r in 0 until ROWS) {
-                mBoardLocations[c][r] =
-                    BoardLocation(c, r)
-            }
+    fun buildBoardLocations() {
+
+        val board = Board()
+        for (location in BoardLocation.all()) {
+            val piece = board.getPiece(location) ?: continue
+
+            addPiece(location.x, location.y, piece)
         }
+
     }
 
     override fun onDraw(canvas: Canvas?) {
+        drawBoard(canvas)
+
+        drawPieces(canvas)
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val x = event.x.toInt()
+        val y = event.y.toInt()
+        var boardLocation: BoardLocation
+        for (i in 0..63) {
+            val c = i % 8
+            val r = i / 8
+            val index = c + (r * 8)
+
+            try {
+                boardLocation = mPieceViews.first { it.location.index == i }.location
+                if (boardLocation.isTouched(x, y)) {
+                    println("alg location ${getAlgebraicPosition(boardLocation.index)}")
+                    boardViewListener?.onLocationTouched(this, boardLocation)
+                }
+            } catch (e: Exception) {
+                println(e)
+            }
+        }
+
+        return true
+    }
+
+    // MARK: - Private
+
+    fun drawBoard(canvas: Canvas?) {
+
         val width = getWidth()
         val height = getHeight()
 
@@ -51,37 +83,84 @@ class BoardView(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
 
         computeOrigins(width, height)
 
-        for (c in 0 until COLS) {
-            for (r in 0 until ROWS) {
-                val xCoord = getXCoord(c)
-                val yCoord = getYCoord(r)
-                val tileRect = Rect(
-                    xCoord,  // left
-                    yCoord,  // top
-                    xCoord + squareSize,  // right
-                    yCoord + squareSize // bottom
-                )
-                mBoardLocations[c][r].setBoardLocationRect(tileRect)
-                mBoardLocations[c][r].draw(canvas!!)
+        for (i in 0..63) {
+            val c = i % 8
+            val r = i / 8
+            val index = c + (r * 8)
+
+            val color =
+                if ((c + r) % 2 == 0) android.graphics.Color.BLUE else android.graphics.Color.BLACK
+
+            val fill = Paint()
+            fill.color = color
+
+            val xCoord = getXCoord(c)
+            val yCoord = getYCoord(r)
+            val tileRect = Rect(
+                xCoord,  // left
+                yCoord,  // top
+                xCoord + squareSize,  // right
+                yCoord + squareSize // bottom
+            )
+
+            canvas!!.drawRect(tileRect, fill)
+
+            try {
+                val pieceView = mPieceViews.first { it.location.index == i }
+                pieceView.location.setBoardLocationRect(tileRect)
+            } catch (e: Exception) {
+                println(e)
             }
+
         }
+
     }
 
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        val x = event.x.toInt()
-        val y = event.y.toInt()
-        var boardLocation: BoardLocation
-        for (c in 0 until COLS) {
-            for (r in 0 until ROWS) {
-                boardLocation = mBoardLocations[c][r]
-                if (boardLocation.isTouched(x, y)) {
-                    println("index: ${boardLocation.index}")
-                    boardViewListener?.onLocationTouched(this, boardLocation)
-                }
-            }
+    fun drawPieces(canvas: Canvas?) {
+
+        for (pieceView in mPieceViews) {
+
+            val gridX = pieceView.location.x
+            val gridY = 7 - pieceView.location.y
+
+            val width = canvas?.width?.div(8)
+            val height = canvas?.height?.div(8)
+
+            val image = pieceView.mPieceImage!!
+
+            val rect = pieceView.location.getBoardLocationRect()
+            image.bounds = Rect(rect.left, rect.top, rect.right, rect.bottom)
+
+            image.draw(canvas!!)
+
+            invalidateDrawable(image)
+
         }
 
-        return true
+    }
+
+    fun getAlgebraicPosition(index: Int): String {
+        val gridX = index % 8
+        val gridY = index / 8
+        val boardAlpha = ('a'..'h').toList()
+
+        return "${boardAlpha[gridX]}${gridY + 1}"
+    }
+
+    fun addPiece(x: Int, y: Int, piece: Piece) {
+
+        val location = BoardLocation(x, y)
+
+        val pieceView = PieceView(piece, location, this.context)
+        mPieceViews.add(pieceView)
+
+    }
+
+    fun updatePieceViewSelectedStates() {
+
+        for (pieceView in mPieceViews) {
+            pieceView.selected = (pieceView.location.index == selectedIndex)
+        }
     }
 
     fun getSquareSizeWidth(width: Int): Int {
@@ -103,6 +182,24 @@ class BoardView(context: Context?, attrs: AttributeSet?) : View(context, attrs) 
     fun computeOrigins(width: Int, height: Int) {
         this.x0 = (width - squareSize * 8) / 2
         this.y0 = (height - squareSize * 8) / 2
+    }
+
+    fun boardIndexForLocation(bounds: Rect, x: Int, y: Int): Int {
+
+        var x = x
+        var y = y
+
+        // Flip y (0 at bottom)
+        y = bounds.height() - y
+
+        // Get Grid coordinates
+        var gridX = (8.0 * x / bounds.width()).toInt()
+        var gridY = (8.0 * y / bounds.height()).toInt()
+        gridX = Math.min(7, gridX)
+        gridY = Math.min(7, gridY)
+
+        // Make board index
+        return gridX + (gridY*8)
     }
 
 }
