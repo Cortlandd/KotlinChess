2e4e2e50804daa86af51ca0e5a67963972b1d115
Cortland Walker <cortlandjwalker@icloud.com>
2020-08-31 22:49:40 -0500

    Added additional GameListener methods for handling movement

--- Engine/src/main/java/com/cortland/kotlinchess/Interfaces/GameListener.kt
+++ Engine/src/main/java/com/cortland/kotlinchess/Interfaces/GameListener.kt
@@ -1,7 +1,15 @@
 package com.cortland.kotlinchess.Interfaces
 
+import com.cortland.kotlinchess.BoardLocation
 import com.cortland.kotlinchess.Game
+import com.cortland.kotlinchess.Piece
 
 interface GameListener {
     fun gameDidChangeCurrentPlayer(game: Game)
+    fun gameWillBeginUpdates(game: Game) // Updates will begin
+    fun gameDidAddPiece(game: Game) // A new piece was added to the board (do we catually need to include this funtionality?)
+    fun gameDidRemovePiece(game: Game, piece: Piece, location: BoardLocation) // A piece was removed from the board
+    fun gameDidMovePiece(game: Game, piece: Piece, toLocation: BoardLocation) // A piece was moved on the board
+    fun gameDidTransformPiece(game: Game) // A piece was transformed (eg. pawn was promoted to another piece)
+    fun gameDidEndUpdates(game: Game) // Updates will end
 }
