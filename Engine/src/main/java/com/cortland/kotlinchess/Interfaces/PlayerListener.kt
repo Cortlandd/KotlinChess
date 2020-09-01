54baf0da3e2fdd7f0568f0e2d23be5cf6f238206
Cortland Walker <cortlandjwalker@icloud.com>
2020-08-31 22:51:23 -0500

    Update PlayerListener to pass in BoardOperations

--- Engine/src/main/java/com/cortland/kotlinchess/Interfaces/PlayerListener.kt
+++ Engine/src/main/java/com/cortland/kotlinchess/Interfaces/PlayerListener.kt
@@ -1,7 +1,8 @@
 package com.cortland.kotlinchess.Interfaces
 
+import com.cortland.kotlinchess.BoardOperation
 import com.cortland.kotlinchess.Player
 
 interface PlayerListener {
-    fun playerDidMakeMove(player: Player)
+    fun playerDidMakeMove(player: Player, boardOperations: ArrayList<BoardOperation>)
 }
